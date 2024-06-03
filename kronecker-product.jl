122d99eaac5b9e31587643e31562e5a298e3d008
ball-3 <92955909+ball-3@users.noreply.github.com>
2024-06-03 13:28:37 -0700

    generalised kronecker-product
    seeems to work for all cases

--- kronecker-product.jl
+++ kronecker-product.jl
@@ -1,38 +1,43 @@
-function kron(matrix1::Matrix, matrix2::Matrix)		#2d arrays only, overload for other cases
-	println(size(matrix1))
-	(r1,c1) = size(matrix1)
-	(r2,c2) = size(matrix2)
-	m = 0
-	n = 0
+function kron(matrices...)
+
+	len = length(matrices)
+
+	if len == 0
+		return nothing
+	elseif len == 1
+		return matrices[1]
+	end
 	
-	#result = Array{Union{Missing, Number}}(missing, r1*r2, c1*c2)
-	result = zeros(r1*r2,c1*c2)
+	rowsA = size(matrices[1],1)
+	columnsA = size(matrices[1],2)
+	matrixA = matrices[1]
 	
-	for j = 1:c1			#bro this is horrible code ??
-		for i = 1:r1
-			n = n%(c1*c2)+1
-			for l = 1:c2
-				n = (j-1)*c2+l
-				for k = 1:r2
-					m = (i-1)*r2+k		#rearrange in loop for optimization
-					result[m,n] = matrix1[i,j]*matrix2[k,l]
+	for i = 2:(len)
+		rowsB = size(matrices[i],1)
+		columnsB = size(matrices[i],2)
+		matrixB = matrices[i]
+		
+		rowsAB = rowsA*rowsB
+		colsAB = columnsA*columnsB
+		matrixAB = zeros(rowsAB,colsAB)
+		
+		cAB = 0
+		for cA = 1:columnsA
+			for cB = 1:columnsB
+				cAB += 1
+				rAB = 0
+				for rA = 1:rowsA
+					for rB = 1:rowsB
+						rAB += 1
+						matrixAB[rAB,cAB] = matrixA[rA,cA]*matrixB[rB,cB]
+					end
 				end
 			end
 		end
+		
+		rowsA = rowsAB
+		columnsA = colsAB
+		matrixA = matrixAB
 	end
-	return result			#^_^
+	return matrixA
 end
-
-function kron(matrix::Matrix, value::Number)
-	return value .* matrix
-end
-
-function kron(value::Number, matrix)
-	return value .* matrix
-end
-
-function kron(value1::Number, value2::Number)
-	return value1*value2
-end
-
-#display(a) to show matrixx
